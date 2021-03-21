b56c3fe6e28011099339ae2d76c0e7dea316f718
mikehandford <mike.handford@audient.com>
2021-03-21 17:45:42 +0000

    Working inteliisense and added win github workflow

--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -4,7 +4,7 @@
 // directly. If you need to remain compatible with Projucer-generated builds, and
 // have called `juce_generate_juce_header(<thisTarget>)` in your CMakeLists.txt,
 // you could `#include <JuceHeader.h>` here instead, to make all your module headers visible.
-#include <juce_gui_extra/juce_gui_extra.h>
+#include <juce_gui_basics/juce_gui_basics.h>
 
 //==============================================================================
 /*
